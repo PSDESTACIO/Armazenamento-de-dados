5db5d5d47e0228ee67d568d2bb1e386b8ece3c8d
Isabelle <crazyflyingzombie@gmail.com>
2024-04-17 18:57:57 -0300

    💬 arrumando o código, e adicionando comentários

--- main_postgre.py
+++ main_postgre.py
@@ -4,13 +4,16 @@ from repositorypostgre.cliente_repository_postgre import ClienteRepositoryPostgr
 
 cliente1 = Cliente(nome="lu",email="lu@gmail.com")
 cliente2 = Cliente(nome="italo",email="italo@gmail.com")
+
 try:
+    # Tenta salvar adicionar os clientes definidos acima na base de dados PostgreSQL.
     repository = ClienteRepositoryPostgre()
     msg = repository.save(cliente1)
     msg2 = repository.save(cliente2)
+
+    # O método save retorna strings com o resultado do procedimento.
     print(f"{msg} , {msg2}")
     repository.findAll()
+
 except Exception as error:
-    print(f" Error : {error}")
-
-
+    print(f"Error : {error}")
